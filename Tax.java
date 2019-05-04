5883007291529ad50eb3aa739af0ce7f07e755a9
Brynn Haley <brynn.haley@live.com>
2019-05-04 00:51:29 -0300

    Added Javadoc.

--- Tax.java
+++ Tax.java
@@ -1,14 +1,34 @@
 import java.util.Map;
 import java.util.HashMap;
 
+/**
+This program calculates sales tax on a user-submitted dollar amount,
+based on the provincial sales tax rate.
+@author Brynn Haley
+*/
 public class Tax{
 	
+	/**
+	The name of a Canadian province, in abbreviated form (ie. NB).
+	*/
 	private String province;
+	/**
+	The original dollar amount before tax.
+	*/
 	private double base;
+	/**
+	The sales tax percentage in a given province, represented as a decimal (ie 15% = 0.15).
+	*/
 	private double rate;
 	
+	/**
+	Stores province names (ie. "NB") as keys and their correspoding tax rates as values in decimal form.
+	*/
 	private HashMap<String, Double> map; 
       
+	/**
+	Populates map with provinces and their tax rates.
+	*/
 	public void populate(){
 		map = new HashMap<String, Double>(); 
 		map.put("AB", 0.05); 
@@ -26,36 +46,48 @@ public class Tax{
 	   map.put("YK", 0.05); 
 	}
 	
-	public Map<String, Double> getMap(){
-		return map;
-	}
-	
+	/**
+	Constructs a Tax object.
+	@param provincee The province's abbreviation (ie. AB).
+	@param base The before-tax dollar amount.
+	*/
 	public Tax(String province, double base){
 		this.province = province;
 		this.base = base;
 	}
 	
+	/** 
+	Sets the province name based on user input.
+	@param province The abbreviated name of the province.
+	*/
 	public void setProvince(String province){
 		this.province = province;
 	}
 	
+	/**
+	Sets the before-tax dollar amount to be calculated for sales tax.
+	@param base The before-tax amount.
+	*/
 	public void setBase(double base){
 		this.base = base;
 	}
 	
-	public String getprovince(){
-		return province;
-	}
-	
-	public double getBase(){
-		return base;
-	}
-	
+	/**
+	Calculates the amount of tax to be paid based on the original dollar amount
+	and the province in which the purchase is being made.
+	@param tax The user-provided province and original before-tax dollar amount.
+	@return (tax.base * tax.rate) The amount of tax to be paid.
+	*/
 	public double calcTax(Tax tax){
 		rate = map.get(tax.province);
 		return tax.base * tax.rate;
 	}
 	
+	/**
+	Calculates the new total amount to be paid with the tax factored in.
+	@param tax The province and original before-tax dollar amount.
+	@return (tax.base *  tax.rate + tax.base) The total amount due after tax.
+	*/
 	public double calcTotal(Tax tax){
 		rate = map.get(tax.province);
 		return tax.base *  tax.rate + tax.base;
--- TaxDriver.java
+++ TaxDriver.java
@@ -5,13 +5,11 @@ public class TaxDriver{
 	public static void main(String[] args){
 
 	Scanner scan = new Scanner(System.in);
-	
-	Tax tax = new Tax("NB",0);
-	
+
 	System.out.print("Enter before-tax dollar amount: ");
 	System.out.println("");
 	
-	tax.setBase(scan.nextDouble());
+	Tax tax = new Tax("", scan.nextDouble());
 	
 	System.out.print("Enter province (ie. NB, MN, AB, etc.): ");
 	System.out.println("");
@@ -22,7 +20,6 @@ public class TaxDriver{
 	
 	System.out.println("Total tax: " + tax.calcTax(tax) + "\n" + 
 		"Total including tax: " + tax.calcTotal(tax));
-	
 	}
 }
 
